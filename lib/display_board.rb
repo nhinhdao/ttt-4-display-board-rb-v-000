4bdec0620443130a007b05f5e53c8455dca7b5db
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-13 03:51:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,7 @@
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
 
-def display_board(board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "])
+def display_board(board = ["","","","","","","","",""])
       puts " #{board[0]} #{'|'} #{board[1]} #{'|'} #{board[2]} "
       puts "-----------"
       puts " #{board[3]} #{'|'} #{board[4]} #{'|'} #{board[5]} "
